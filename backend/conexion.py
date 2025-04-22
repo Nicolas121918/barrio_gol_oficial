a4c05aa315f62110303a43149ac670496d032e7f
Johan024 <jevergara987@misena.edu.co>
2025-04-22 13:39:20 -0500

    Correcciones generales

--- backend/conexion.py
+++ backend/conexion.py
@@ -2,11 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-<<<<<<< HEAD
-URL_DB = "mysql+mysqlconnector://root:231219@localhost:3306/registro"
-=======
 URL_DB = "mysql+mysqlconnector://root:nomelase123@localhost:3306/registro"
->>>>>>> d9034e8 (Correcciones generales)
 
 engine = create_engine(URL_DB)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
